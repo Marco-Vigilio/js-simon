547f6e2cae6aac1f8555ba26fea8ecab962bedc8
Marco-Vigilio <tonyvigilio@gmail.com>
2023-05-26 01:33:12 +0200

    fermo il setInterval quando CountDown è 0

--- script/myscript.js
+++ script/myscript.js
@@ -12,11 +12,11 @@ Da quanti millisecondi è composto un secondo?
 Quanti millisecondi mi separano da domani alle 9:30?
 Esiste un oggetto JS in grado di gestire le date?
 
-Esistono dei metodi per trasformare una data in millisecondi?
+* * Esistono dei metodi per trasformare una data in millisecondi?
 
  * Traccia
-Sfruttiamo le timing functions per fare il conto alla rovescia per la correzione di domani!
-Ogni secondo il nostro countdown dovrà scalare fino alle 9:30 di domani mattina!
+* * Sfruttiamo le timing functions per fare il conto alla rovescia per la correzione di domani!
+* * Ogni secondo il nostro countdown dovrà scalare fino alle 9:30 di domani mattina!
  * 
  * 
  */
@@ -55,47 +55,54 @@ function orologio(){
 }
 */
 
-setInterval(countDown, 1000);
+let clock = setInterval(countDown, 1000);
 
 function countDown(){
     const adesso = new Date();
-    console.log(adesso);
-
     const dopo = new Date("May 26, 2023 09:30:00");
-    console.log(dopo);
     const countDown = dopo - adesso;
-    console.log(countDown); //RESTITUISCE IN MILLESIMI DI SECONDO 
+    //console.log(countDown); //RESTITUISCE IN MILLESIMI DI SECONDO 
 
     //CALCOLO DEI GIORNI - ORE - MINUTI - SECONDI
     let days = Math.floor(countDown / (1000 * 60 * 60 * 24));
-    console.log(days);
 
     let hours = Math.floor((countDown % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-    console.log(hours);
 
     let minutes = Math.floor((countDown % (1000 * 60 * 60)) / (1000 * 60));
-    console.log(minutes);
 
     let seconds = Math.floor((countDown % (1000 * 60)) / 1000);
-    console.log(seconds);
 
     if(!(countDown === 0)){
 
-    //FACCIO VEDERE IN PAGINA I VALORI
-    let dayTime = document.querySelector("p.day");
-    dayTime.innerHTML = days;
+        //FACCIO VEDERE IN PAGINA I VALORI
+        let dayTime = document.querySelector("p.day");
+        dayTime.innerHTML = days;
 
-    let hourTime = document.querySelector("p.hour");
-    hourTime.innerHTML = hours;
+        let hourTime = document.querySelector("p.hour");
+        hourTime.innerHTML = hours;
 
-    let minuteTime = document.querySelector("p.minute");
-    minuteTime.innerHTML = minutes;
+        let minuteTime = document.querySelector("p.minute");
+        minuteTime.innerHTML = minutes;
 
-    let secondTime = document.querySelector("p.second");
-    secondTime.innerHTML = seconds;
+        let secondTime = document.querySelector("p.second");
+        secondTime.innerHTML = seconds;
 
     }
     else{
-        alert("Oggi c'e Classe");
+        
+        let div = document.querySelector("div.flex");
+        div.innerHTML = "";
+        let divTodayClass = CreateElement("p", "today-class");
+        divTodayClass.append("Today is the day !!!");
+        div.append(divTodayClass);
+        console.log("Oggi c'è classe");
+        clearInterval(clock);
     }
+}
+
+
+function CreateElement(tagName, className){
+    let elemento = document.createElement(tagName);
+    elemento.classList.add(className);
+    return elemento;
 }
